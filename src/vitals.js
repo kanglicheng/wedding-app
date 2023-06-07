7452093383502fdffb26b8283bb30faebe49000d
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-06-07 16:26:44 -0400

    update

--- src/vitals.js
+++ src/vitals.js
@@ -11,7 +11,7 @@ function getConnectionSpeed() {
 }
 
 export function sendToVercelAnalytics(metric) {
-  const analyticsId = process.env.REACT_APP_VERCEL_ANALYTICS_ID;
+  const analyticsId = process.env.VERCEL_ANALYTICS_ID;
   console.log(analyticsId, 'analyticsID')
   if (!analyticsId) {
     return;
