418d0afcc3e01a339889d712c0933cc59a86a428
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-06-09 22:00:54 -0400

    add header image

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -9,7 +9,7 @@ export const Navigation = () => {
 	return (
 		<div style={{ margin: '15px' }}>
 			<div style={{ textAlign:"center", color:"greenyellow"}}>
-				<img className={'headerImage'} alt={"header"} src={"https://xesldvbvamzutxnexmdj.supabase.co/storage/v1/object/public/photos/dartmouth-kccheng.jpeg"}/>
+				<img className={'headerImage'} alt={"header"} src={"images/flowers.png"}/>
 				<h1><a className={'title'} href={"/"}>KANG-LI CHENG & YUE TU</a></h1>
 			</div>
 			<nav>
