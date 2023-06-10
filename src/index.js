ba1b6f9fdfbb933faf6f7943911daae1a7d7a0f4
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-06-10 10:59:26 -0400

    enable analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import { ErrorPage } from './ErrorPage';
 import './index.css';
 import { Navigation } from './Navigation';
@@ -43,7 +43,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
-		{/* <Analytics/> */}
+		<Analytics/>
 	</React.StrictMode>
 );
 
