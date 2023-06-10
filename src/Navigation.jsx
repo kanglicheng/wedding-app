1390c0fbf698c1c9daf7f78542258d2a5214e108
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-06-09 20:51:59 -0400

    mobile

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -10,7 +10,7 @@ export const Navigation = () => {
 		<div style={{ margin: '15px' }}>
 			<div style={{ textAlign:"center", color:"greenyellow"}}>
 				<img className={'headerImage'} alt={"header"} src={"https://xesldvbvamzutxnexmdj.supabase.co/storage/v1/object/public/photos/dartmouth-kccheng.jpeg"}/>
-				<h1><a style={{textDecoration: "none", color:"#4e4d52", fontWeight: "400", letterSpacing: "8px", lineHeight: "1", fontSize: "48px", fontFamily:'Nanum Myeongjo'  }} href={"/"}>KANG-LI CHENG & YUE TU</a></h1>
+				<h1><a className={'title'} href={"/"}>KANG-LI CHENG & YUE TU</a></h1>
 			</div>
 			<nav>
 				<div
