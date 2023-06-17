465dd8271bf22884a335a11e70646c4877421521
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-06-16 21:10:39 -0400

    fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,10 +8,13 @@ export const Home = () => {
 	return (
 		<div style={{margin: "auto", display: "flex", flexDirection: "column", alignItems: "center", padding: "5px"}}>
 			<h2>Welcome!</h2>
-			<p>
-				Thank you for stopping by. We are getting married this year! This page contains all the information about our wedding 
-				day. Please <a href={'/rsvp'}>RSVP</a> should you be interested in attending!
-			</p>
+			<div style={{maxWidth: "35em"}}>
+				<p style={{textAlign: "center"}}>
+					Thank you for visiting Yue and Kang-Li&apos;s wedding website. This page contains all the information about our wedding 
+					day. Please <a href={'/rsvp'}>RSVP</a> should you be interested in attending!
+				</p>
+			</div>
+			
 			<div style={{margin: "auto"}}>
 				<h2>Event Information</h2>
 					<h3>Date and Time</h3>
@@ -33,7 +36,7 @@ export const Home = () => {
 							Reception: <a href={"https://shakermuseum.org/"}>Shaker Museum</a> in Enfield, NH
 						</p>
 							<b>Address</b> 
-							<div style={{display: "flex", justifyContent: "center"}}>
+							<div>
 							<ClipboardCopy hoverTip={'Click to copy!'} exitDelay={2000} style={{width: "400px", backgroundColor:"lightgoldenrodyellow"}} position={"top-start"}isReadOnly>
 									447 NH Route 4A, Enfield, New Hampshire 03748 
 							</ClipboardCopy>
@@ -41,8 +44,10 @@ export const Home = () => {
 	
 					</section>
 					<figure>
-						<img className={"image"} src={"https://gyesgoqhxmpfhaqfvklq.supabase.co/storage/v1/object/public/property-photos/shaker.jpeg"}/>
-						<figcaption>Shaker Museum, photo taken May 2023</figcaption>
+						<img className={"image"} src="https://xesldvbvamzutxnexmdj.supabase.co/storage/v1/object/public/photos/shaker1.jpeg?t=2023-06-17T00%3A57%3A48.081Z"/>
+						<figcaption>
+							Shaker Museum, photo taken June 15th, 2023
+						</figcaption>
 					</figure>
 			</div>
 
--- src/Pictures.jsx
+++ src/Pictures.jsx
@@ -9,6 +9,14 @@ export const Pictures = () => {
             <img className={"image"} alt={'alt text'} src={"https://gyesgoqhxmpfhaqfvklq.supabase.co/storage/v1/object/public/property-photos/yue-ivy.png?t=2023-06-12T03%3A19%3A13.368Z"} />
             <figcaption>Coming Soon!</figcaption>
           </figure>
+          <figure>
+						<img className={"image"} src={"https://gyesgoqhxmpfhaqfvklq.supabase.co/storage/v1/object/public/property-photos/shaker.jpeg"}/>
+						<figcaption>Shaker Museum, photo taken May 2023</figcaption>
+					</figure>
+					<figure>
+						<img className="image" src="https://xesldvbvamzutxnexmdj.supabase.co/storage/v1/object/public/photos/shaker2.jpeg?t=2023-06-17T01%3A00%3A05.822Z"/>
+						<figcaption>June 15th, 2023</figcaption>
+					</figure>
         </div>
         )
 }
