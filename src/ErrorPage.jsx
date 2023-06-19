b9e119484240734a0cc4171e0b716437fa44182e
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-06-18 22:20:44 -0400

    mini fix

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const ErrorPage = () => {
 	return (
 		<div>
-			<h2>I didn&apos;t have time to build a property error page</h2>
+			<h2>I didn&apos;t have time to build a proper error page!</h2>
 			<h3>
-        Please go back using the back button or <a href="/">Click here</a>
+             Please go back using the back button or <a href="/">Click here</a>
 			</h3>
 		</div>
 	);
